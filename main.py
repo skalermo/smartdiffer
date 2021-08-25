cdf57d30c35390c91cc46f5f30a5b1c7e02e5a03
Roman Moskalenko <skalermo@gmail.com>
2021-08-25 11:43:33 +0200

    Accept files as sources

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import json
 
 import requests
@@ -7,11 +8,27 @@ from etherscan.contracts import Contract
 
 WEB_URL = 'https://www.diffchecker.com/'
 API_ENDPOINT = 'https://diffchecker-api-production.herokuapp.com'
+ETHERSCAN_API_KEY = ''
 AUTH_TOKEN = ''
 
 
 def is_address(string: str):
     return string.startswith('0x')
+
+
+def retrieve_code(source: str) -> str:
+    code = ''
+    if is_address(source):
+        query_result = Contract(address=source, api_key=ETHERSCAN_API_KEY).get_sourcecode()
+        source_code_json = json.loads(query_result[0]['SourceCode'][1:-1])
+        sources_sorted = sorted(source_code_json['sources'].keys())
+        code = '\n'.join(source_code_json['sources'][src]['content'] for src in sources_sorted)
+    else:
+        # if not address assume it is path to file with code
+        path = os.path.realpath(source)
+        with open(path, 'r') as f:
+            code = f.read()
+    return code
 
 
 def make_post_req(left: str = '', right: str = ''):
@@ -31,30 +48,23 @@ def main():
         print(f'Usage: python {sys.argv[0]} address/on/etherscan')
         exit(0)
 
-    addresses = sys.argv[1:]
-    sources = ['' for _ in range(len(addresses))]
+    sources = sys.argv[1:]
+    codes = ['' for _ in range(len(sources))]
 
     with open('api_keys.json', mode='r') as key_file:
         keys = json.loads(key_file.read())
-        key = keys['etherscan']
+        ETHERSCAN_API_KEY = keys['etherscan']
         AUTH_TOKEN = keys['diffchecker']
 
-    for i, address in enumerate(addresses):
-        contract = Contract(address=address, api_key=key)
-        sourcecode = contract.get_sourcecode()
+    for i, source in enumerate(sources):
+        code = retrieve_code(source)
 
-        if not sourcecode:
-            print('No source code found')
-            return
+        if not code:
+            code = f'Cannot retrieve source code from source "{source}" !!!'
 
-        query_result = json.loads(sourcecode[0]['SourceCode'][1:-1])
-        sources_sorted = sorted(query_result['sources'].keys())
+        codes[i] = code
 
-        sources[i] = '\n'.join(query_result['sources'][src]['content'] for src in sources_sorted)
-        # with open(address, 'w') as f:
-        #     f.write(source)
-
-    left, right = sources[0:2]
+    left, right = codes[0:2]
 
     response = make_post_req(left, right)
     url = WEB_URL + json.loads(response.text)['slug']
