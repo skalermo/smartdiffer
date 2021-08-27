f4711ff2753cfebf798e9ebbe13ad808a6b769ed
skalermo <skalermo@gmail.com>
2021-08-28 01:08:46 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Tool to compare smart contracts source code'
+
+setup(
+    name="smartdiff",
+    version=VERSION,
+    author="skalermo (Roman Moskalenko)",
+    author_email="skalermo@gmail.com",
+    description=DESCRIPTION, 
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=['requests', 'py-etherscan-api'],
+    keywords=['python', 'etherscan-api', 'diffchecker'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    url='https://github.com/skalermo/morphine',
+    entry_points={
+        'console_scripts': [
+            'smartdiff=smartdiff.cli:main',
+        ],
+    },
+)
