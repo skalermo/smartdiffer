37d0cbeaeba012f1d4ed2a021bd2cc59afb7b1cb
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2023-02-26 11:35:59 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 DESCRIPTION = 'Tool to compare smart contracts source code'
 
 setup(
